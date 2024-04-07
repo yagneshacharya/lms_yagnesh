af0fe7517d970987331732c162c6451b11edf956
Shourya Rami <shouryarami@gmail.com>
2024-04-07 12:42:44 +0530

    "skill id added"

--- Model/Skill_model.js
+++ Model/Skill_model.js
@@ -1,25 +1,29 @@
 const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
-
+const { nanoid } = require("nanoid");
 let Skill_registration = mongoose.Schema({
+  Skill_id: {
+    type: String,
+    default: () => nanoid(5),
+  },
   Skill_name: {
     type: String,
   },
   Skill_Description: {
     type: String,
   },
-  Sub_skills:{
-    type : Array
+  Sub_skills: {
+    type: Array,
   },
   Skill_isDeleted: {
     type: Boolean,
     default: false,
   },
-  company_id : {
-     type : mongoose.Schema.Types.ObjectId,
-     ref : 'company_db',
-     required : true
-  }
+  company_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "company_db",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Skill_Model", Skill_registration);
